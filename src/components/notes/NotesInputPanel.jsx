40c40c1dad95850d5b729ded4f602a7ca277f9ae
Afeef <afeef9706@gmail.com>
2023-08-20 20:32:12 +0800

    Created notes input panel

---
+++ src/components/notes/NotesInputPanel.jsx
@@ -0,0 +1,39 @@
+import { useAtom } from "jotai";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { videoTimeAtom } from "../../atoms";
+import Button from "../shared/Button";
+
+const NotesInputPanel = () => {
+    const [videoTime] = useAtom(videoTimeAtom);
+    const { register, handleSubmit, watch } = useForm();
+
+    const onSubmit = (data, event) => {
+        event.preventDefault();
+        if (data.notes) {
+            console.log(data);
+        }
+    };
+
+    return (
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col items-center justify-between w-[450px] h-full bg-secondaryBG rounded p-8 text-black">
+            <div className="w-full">
+                <h2>Notes</h2>
+                <progress max={1} value={videoTime.played}></progress>
+
+                <textarea
+                    className="w-full rounded p-3 max-h-[290px] min-h-[150px] outline-none border border-secondaryAccent"
+                    placeholder="Write down your notes here"
+                    {...register("notes")}
+                />
+            </div>
+            <div>
+                <Button variant={"solid"}>Create a note</Button>
+            </div>
+        </form>
+    );
+};
+
+export default NotesInputPanel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Header from "../components/Header";
 import VideoPlayer from "../components/video/VideoPlayer";
+import NotesInputPanel from "../components/notes/NotesInputPanel";
 
 const Home = () => {
     return (
         <div className={`w-screen h-screen bg-mainBG`}>
             <Header />
-
-            <VideoPlayer />
+            <div className="h-28"></div>
+            <div className="h-1/2 w-screen flex justify-center items-center">
+                <div className="flex justify-center items-center gap-6">
+                    <VideoPlayer />
+                    <NotesInputPanel />
+                </div>
+            </div>
         </div>
     );
 };
