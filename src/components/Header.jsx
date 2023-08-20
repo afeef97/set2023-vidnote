445817c6eaacc88c48e465bf8e5164c7123e2109
Afeef <afeef9706@gmail.com>
2023-08-21 02:00:19 +0800

    Changed color and sizes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ import IconedInput from "./shared/IconedInput";
 const Header = () => {
     return (
         <div
-            className={`flex justify-between items-center fixed px-24 w-full h-28 bg-mainBG border-b-2 border-b-mainAccent`}>
+            className={`flex justify-between items-center fixed px-24 w-full h-28 bg-mainBG border-b-2 border-b-mainAccent z-50`}>
             <div>
-                <h3 className="text-4xl">VidNote</h3>
+                <h3 className="text-4xl text-white">VidNote</h3>
             </div>
             <IconedInput
                 type={"url"}
