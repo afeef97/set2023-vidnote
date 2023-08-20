f28a81e1155a60043fd375c2097dd4517d56680e
Afeef <afeef9706@gmail.com>
2023-08-20 15:28:28 +0800

    Added custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                mainBG: "#000814",
+                secondaryBG: "#001D3D",
+                mainAccent: "#FFD60A",
+                secondaryAccent: "#FFC300",
+                tertiaryAccent: "#003566",
+                mainText: "#FFFFFF",
+            },
+        },
     },
     plugins: [],
 };
