55b2421edd8ab458383fc1d0f7ab1250abeebed0
Afeef <afeef9706@gmail.com>
2023-08-21 10:08:56 +0800

    Changed height for Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,15 +6,16 @@ import NotesPanel from "../components/notes/NotesPanel";
 
 const Home = () => {
     return (
-        <div className={`w-screen h-screen bg-mainBG`}>
+        <div
+            className={`w-screen pb-12 h-[1300px] flex justify-between flex-col gap-12 bg-mainBG`}>
             <Header />
             <div className="h-28"></div>
             <div className="h-[90%] w-full flex flex-col gap-6 justify-center items-center">
-                <div className="w-5/6 max-w-[1328px] flex justify-center items-center gap-6">
+                <div className="w-5/6 max-w-[1328px] h-full flex justify-center items-center gap-6">
                     <VideoPlayer />
                     <NotesInputPanel />
                 </div>
-                <div className="relative w-5/6 max-w-[1328px] basis-1/2">
+                <div className="w-5/6 max-w-[1328px]">
                     <NotesPanel />
                 </div>
             </div>
